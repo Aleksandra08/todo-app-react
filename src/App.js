567863a7280508db77176f77496f89231941b500
Aleksandra <starodub.alexandra@gmail.com>
2019-07-08 18:12:37 +0300

    result

--- src/App.js
+++ src/App.js
@@ -2,68 +2,116 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from "./components/Footer";
 
-// import Footer from "./components/Footer";
 
+const FIELD = {
+  all: 1,
+  active: 2,
+  completed: 3
+};
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            itemsList: [],
-        }
+  constructor() {
+    super();
+    this.state = {
+      itemsList: [],
+      filterField: FIELD.all,
+      checked: false
     }
+  }
 
-    changeItem = (id) => {
-        const todos = [...this.state.itemsList];
-        const index = todos.findIndex((item) => item.id === id);
-        todos[index] = {
-            ...todos[index],
-            completed: !todos[index].completed
-        };
-        this.setState({itemsList: todos})
+  changeItem = (id) => {
+    const todos = [...this.state.itemsList];
+    const index = todos.findIndex((item) => item.id === id);
+    todos[index] = {
+      ...todos[index],
+      completed: !todos[index].completed
     };
+    this.setState({itemsList: todos})
+  };
 
-    addItem = (item) => {
-        if (!item) return;
-        const todoItem = {
-            id: Date.now(),
-            title: item,
-            completed: false
-        };
-        this.setState((prev) => ({
-                itemsList: [
-                    ...prev.itemsList,
-                    todoItem
-                ]
-            }
-        ))
+  addItem = (item) => {
+    if (!item) return;
+    const todoItem = {
+      id: Date.now(),
+      title: item,
+      completed: false
     };
+    this.setState((prev) => ({
+        itemsList: [
+          ...prev.itemsList,
+          todoItem
+        ]
+      }
+    ))
+  };
 
-    removeItem = (id) => {
-        this.setState((prev) => {
-            let copyList = [...prev.itemsList];
-            copyList.splice(id, 1);
-            return {
-                itemsList:
-                    prev.itemsList.filter((item) => item.id !== id)
-            }
-        })
-    };
+  removeItem = (id) => {
+    this.setState((prev) => {
+      let copyList = [...prev.itemsList];
+      copyList.splice(id, 1);
+      return {
+        itemsList:
+          prev.itemsList.filter((item) => item.id !== id),
+      }
+    })
+  };
 
-    render() {
-        return (
-            <div className="todoapp">
-                <Header addItem={this.addItem}/>
-                <Main
-                    items={this.state.itemsList}
-                    removeItem={this.removeItem}
-                    change={this.changeItem}
-                />
-                {/*<Footer/>*/}
-            </div>
-        )
+  changeFilterBy = (field) => {
+    this.setState({filterBy: field})
+  };
+
+  onFilter = (field) => {
+    switch (field) {
+      case FIELD.all: {
+        return this.state.itemsList
+      }
+      case FIELD.active: {
+        return this.state.itemsList.filter((item) => !item.completed)
+      }
+      case FIELD.completed: {
+        return this.state.itemsList.filter((item) => item.completed)
+      }
+      default: {
+        return this.state.itemsList
+      }
     }
+  };
+
+  checkedAllItems = () => {
+    const listOfItems = this.state.itemsList.map((item) => ({
+      ...item,
+      completed: !this.state.checked,
+    }));
+
+    this.setState({
+      itemsList: listOfItems,
+      checked: !this.state.checked
+    })
+  };
+
+
+  render() {
+    const filteredItems = this.onFilter(this.state.filterBy);
+    return (
+      <div className="todoapp">
+        <Header addItem={this.addItem}/>
+        <Main
+          items={filteredItems}
+          removeItem={this.removeItem}
+          change={this.changeItem}
+          checkedAllItems={this.checkedAllItems}
+        />
+        <Footer
+          count={filteredItems.length}
+          filterItems={this.changeFilterBy}
+          filterField={FIELD}
+          showCurrentList={this.state.filterBy}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
